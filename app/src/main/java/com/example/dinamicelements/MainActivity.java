49ec09371849f4234493bae02be1f974bd1ad63e
OutSocium <gorshkov-ev@yandex.ru>
2024-02-15 12:57:12 +0300

    with layout checkBox button

--- app/src/main/java/com/example/dinamicelements/MainActivity.java
+++ app/src/main/java/com/example/dinamicelements/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.dinamicelements;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
@@ -18,6 +23,27 @@ public class MainActivity extends AppCompatActivity {
         imageView.setImageResource(R.drawable.ic_launcher_background);
         LayoutParams imageViewLayoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         imageView.setLayoutParams(imageViewLayoutParams);
-        linearlayout.addView(imageView);
+        //linearlayout.addView(imageView);
+       CheckBox enableLayer1 = (CheckBox) findViewById(R.id.enlayer1);
+
+
+        LinearLayout mainLayer = (LinearLayout) findViewById(R.id.master);
+
+        LayoutInflater inflater = getLayoutInflater();
+       View layer1 = inflater.inflate(R.layout.layout1, null);
+
+       enableLayer1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               if (isChecked) {
+                   mainLayer.addView(layer1);
+               } else {
+                   mainLayer.removeView(layer1);
+               }
+           }
+       });
+
+       Button btn = (Button) findViewById(R.id.button2);
+
     }
 }
